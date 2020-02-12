78eab27ca0e4946a093df1f88e9ddc201a2147dc
jecestanislao10 <jestanislao@stratpoint.com>
2020-02-12 13:41:12 +0800

    new fix

--- test/features/api/users/2.  LoginUsers.spec.js
+++ test/features/api/users/2.  LoginUsers.spec.js
@@ -4,20 +4,25 @@ const request = require('supertest');
 const { expect } = require('chai');
 const { getUserCredentials } = require ('test/support/userHelper');
 const mochaAsync = require('test/support/mochaAsync');
+const { generateUser } = require('test/support/userFactory');
 
 
 describe('API :: POST /api/login', () => {
+
+  before(() => {
+    mochaAsync(generateUser('user'));
+  });
 
   context('when credentials are valid', () => {
     it('returns 200', mochaAsync(async () => { 
       let res = await request('localhost:' + process.env.PORT)
         .post('/api/login')
-        .send(getUserCredentials());
+        .send(getUserCredentials().credentials);
       expect(res.status).to.equal(200);
     }));
     it('returns token with userId', mochaAsync(async () => {
       let res = await request('localhost:' + process.env.PORT).post('/api/login')
-        .send(getUserCredentials());
+        .send(getUserCredentials().credentials);
       const obj = JSON.parse(res.text);
       setToken(obj.details.result.token);
       setUserId(obj.details.result.userId);
