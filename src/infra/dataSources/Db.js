cc64563902279256d8837a7741ced9f417272ac0
Jorelle Agustin <jagustin@stratpoint.com>
2020-02-13 14:56:37 +0800

    added api/docs to unless

--- src/infra/dataSources/Db.js
+++ src/infra/dataSources/Db.js
@@ -1,4 +1,4 @@
-
+// FOR PRODUCTION
 module.exports = {
   name: 'db',
   connector : 'sql',
@@ -12,3 +12,17 @@ module.exports = {
 
   }
 };
+//FOR TESTING
+// module.exports = {
+//   name: 'test-db',
+//   connector : 'sql',
+//   config: {
+//     host: 'remotemysql.com',
+//     username: 'I3pzK8hI33',
+//     password: 'i2kDmahSWR',
+//     database: 'I3pzK8hI33',
+//     dialect: 'mysql',
+//     isSync: 'true',
+
+//   }
+// };
