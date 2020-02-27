ac2f8529d3644bd2dd7aa424b2e6573b080aa945
Jorelle Agustin <jagustin@stratpoint.com>
2020-02-27 14:56:28 +0800

    fixed db issues

--- src/infra/dataSources/Db.js
+++ src/infra/dataSources/Db.js
@@ -1,28 +1,28 @@
 // FOR PRODUCTION
-module.exports = {
-  name: 'db',
-  connector : 'sql',
-  config: {
-    host: 'remotemysql.com',
-    username: 'gsHgUjN4jj',
-    password: 'cK4rfSzeoj',
-    database: 'gsHgUjN4jj',
-    dialect: 'mysql',
-    isSync: 'true',
-
-  }
-};
-//FOR TESTING
 // module.exports = {
-//   name: 'test-db',
+//   name: 'db',
 //   connector : 'sql',
 //   config: {
 //     host: 'remotemysql.com',
-//     username: 'I3pzK8hI33',
-//     password: 'i2kDmahSWR',
-//     database: 'I3pzK8hI33',
+//     username: 'gsHgUjN4jj',
+//     password: 'cK4rfSzeoj',
+//     database: 'gsHgUjN4jj',
 //     dialect: 'mysql',
 //     isSync: 'true',
 
 //   }
 // };
+//FOR TESTING
+module.exports = {
+  name: 'db',
+  connector : 'sql',
+  config: {
+    host: 'remotemysql.com',
+    username: 'I3pzK8hI33',
+    password: process.env.DB_PASSWORD_TEST,
+    database: 'I3pzK8hI33',
+    dialect: 'mysql',
+    isSync: 'true',
+
+  }
+};
